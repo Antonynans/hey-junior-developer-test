104c9d3acc18b320f67118b8aaedbb66178fea04
antonynans <antonynans@gmail.com>
2022-12-31 13:07:21 +0100

    updated the cart page design

--- src/components/pages/cart.js
+++ src/components/pages/cart.js
@@ -1,53 +1,52 @@
-import React, { Component } from 'react'
-import { CartContextConsumer } from '../../context/cartContext'
-import Navbar from '../navbar'
-import { Div, H1 } from './cartstyle'
+import React, { Component } from "react";
+import { CartContextConsumer } from "../../context/cartContext";
+import Navbar from "../navbar";
+import { Button, Div, H1, Span } from "./cartstyle";
 import CartItemCard from "../cardItemCart";
-import CartFooter from '../cart_footer';
-
+import CartFooter from "../cart_footer";
+import { Link } from "react-router-dom";
 
 export default class cart extends Component {
   render() {
     return (
       <>
-                <Navbar />
-                <Div className='container'>
-                <H1>Cart</H1>
+        <Navbar />
+        <Div className="container">
+          <H1>Cart</H1>
 
-                <CartContextConsumer>
-          {({ cart, getTotalInCartItemsQuantity, getTotalPrice }) => (
-            <>
-              {getTotalInCartItemsQuantity > 0 ? (
-                <>
-                  {cart.map((product, index) => (
-                    <CartItemCard
-                      cartPage
-                      key={index}
-                      product={product}
-                      index={index}
-                      cart={cart}
+          <CartContextConsumer>
+            {({ cart, getTotalInCartItemsQuantity, getTotalPrice }) => (
+              <>
+                {getTotalInCartItemsQuantity > 0 ? (
+                  <>
+                    {cart.map((product, index) => (
+                      <CartItemCard
+                        cartPage
+                        key={index}
+                        product={product}
+                        index={index}
+                        cart={cart}
+                      />
+                    ))}
+
+                    <CartFooter
+                      quantity={getTotalInCartItemsQuantity}
+                      total={getTotalPrice}
                     />
-                  ))}
-
-                  <CartFooter
-                    quantity={getTotalInCartItemsQuantity}
-                    total={getTotalPrice}
-                  />
-                </>
-              ) : (
-                // <StyledEmptyCartContainer>
-                //   <span> Your cart is empty &nbsp; (´。＿。｀)</span>
-                //   <Link to="/">
-                //     <StyledShopNowBtn> Shop now </StyledShopNowBtn>
-                //   </Link>
-                // </StyledEmptyCartContainer>
-                ''
-              )}
-            </>
-          )}
-        </CartContextConsumer>
-</Div>
+                  </>
+                ) : (
+                  <Div className="emptyCart">
+                    <Span> Your cart is empty </Span>
+                    <Link to="/">
+                      <Button> Shop now </Button>
+                    </Link>
+                  </Div>
+                )}
+              </>
+            )}
+          </CartContextConsumer>
+        </Div>
       </>
-    )
+    );
   }
 }
--- src/components/pages/cartstyle.jsx
+++ src/components/pages/cartstyle.jsx
@@ -1,9 +1,7 @@
 import styled from "styled-components/macro";
+import { btnColor } from "../addToCartStyle";
 
-export  const H1 = styled.h1`
-  
-
-`
+export const H1 = styled.h1``;
 
 export const Div = styled.div`
   &.container {
@@ -12,4 +10,24 @@ export const Div = styled.div`
     flex-flow: column;
     justify-content: space-between;
   }
-  `;
+  &.emptyCart {
+    display: flex;
+    flex-flow: column;
+  }
+`;
+
+export const Span = styled.span`
+  margin-bottom: 8px;
+  font-weight: 400;
+  font-size: 24px;
+`;
+
+export const Button = styled.button`
+  height: 3.25rem;
+  padding: 0 4rem;
+  border: none;
+  background-color: ${btnColor};
+  color: white;
+  cursor: pointer;
+  margin-top: 2rem;
+`;
